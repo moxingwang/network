9dd596ff4c193e16f32ebf00cd2ba17ac4bbdcf5
xingwang.mo <xingwang.mo@chinaredstar.com>
2018-06-03 18:47:23 +0800

    commit

---
+++ src/main/java/com/mo/network/netty/DiscardServerHandler.java
@@ -0,0 +1,29 @@
+package com.mo.network.netty;
+
+
+import com.alibaba.fastjson.JSON;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * @author MoXingwang on 2018/6/3.
+ */
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter { // (1)
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
+        // Discard the received data silently.
+        ((ByteBuf) msg).release(); // (3)
+        System.out.println(JSON.toJSONString(msg));
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+
+}
